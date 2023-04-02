b60f1427cb73505765bf5e8f195dd8b17e09db05
just-hoshinon <mwadman2018@gmail.com>
2023-04-03 00:47:30 +0300

    feature: added new endpoint that returns two random matrixes 10x10 and their production

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,6 @@
+import numpy
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
@@ -6,3 +8,15 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrixes')
+def get_matrix():
+    A = numpy.random.random_integers(0, 5, (10, 10))
+    B = numpy.random.random_integers(0, 5, (10, 10))
+    product = A.dot(B).tolist()
+    return JSONResponse(content={
+        'matrix_A': A.tolist(),
+        'matrix_B': B.tolist(),
+        'product': product,
+    })
